c2ba22d62f80308f198a6869f087a8ca561f6b90
peteruithoven <peter@peteruithoven.nl>
2016-01-04 01:07:05 +0100

    Resilient network sketch updates

--- resilient-network.ino
+++ resilient-network.ino
@@ -1,5 +1,5 @@
 // resilient network
-#include <SoftwareSerialWithHalfDuplex.h> // which one? 
+#include <SoftwareSerial.h>
 #include "Arduino.h"
 #include "timers.h"
 
@@ -16,12 +16,14 @@
 #define DROPPING_CONNECTOR 254
 #define ALERT 255
 
-SoftwareSerialWithHalfDuplex* bus[3];
+#define NUM_CONN 3
 
-int RX[3] = {A0, A2, A4};
-int TX[3] = {A1, A3, A5};
-int coil[3] = {6, 5, 10}; // magnets
-int distanceToSource[3];
+SoftwareSerial* bus[NUM_CONN];
+
+int RX[NUM_CONN] = {A1, A3, A5};
+int TX[NUM_CONN] = {A0, A2, A4};
+int coil[NUM_CONN] = {6, 5, 10}; // magnets
+int distanceToSource[NUM_CONN];
 int shortestDistanceToSource;
 int lampState;
 int alertState;
@@ -30,7 +32,7 @@ int alertTimer;
 void sendEnergy() {
   if(SERIAL_DEBUG) Serial.println("send energy");
   char c = ENERGY;
-  for (int i=0; i<3; i++) bus[i]->write(c);
+  for (int i=0; i<NUM_CONN; i++) bus[i]->write(c);
   addTimer(100, sendEnergy);
 }
 
@@ -47,17 +49,17 @@ void switchLampOff() {
 }
 
 void resetConnectors() {
-  for (int i=0; i<3; i++) digitalWrite(coil[i], HIGH);
+  for (int i=0; i<NUM_CONN; i++) digitalWrite(coil[i], HIGH);
 }
 
 void dropConnector() {
   // send out message that we're going to drop a connector
   char c = DROPPING_CONNECTOR;
-  for (int i=0; i<3; i++) bus[i]->write(c);
+  for (int i=0; i<NUM_CONN; i++) bus[i]->write(c);
   
   // pick random connector to drop
   int n = 0;
-  for (int i=0; i<3; i++) if (distanceToSource[i]<99) n++;
+  for (int i=0; i<NUM_CONN; i++) if (distanceToSource[i]<99) n++;
   n = random(1,n+1);
   int i=0;
   while(n) {
@@ -77,39 +79,41 @@ void dropConnector() {
 void processEnergy() {
   // assess energy supply
   shortestDistanceToSource = 99;
-  for (int i =0; i<3; i++) if (distanceToSource[i] < shortestDistanceToSource) shortestDistanceToSource = distanceToSource[i];
+  for (int i =0; i<NUM_CONN; i++) if (distanceToSource[i] < shortestDistanceToSource) shortestDistanceToSource = distanceToSource[i];
   
   // if we have energy, switch (or keep) lamp on, pass it on and check again in some time
   if (shortestDistanceToSource<99) {
     if (!lampState) switchLampOn();
-    for (int i =0; i<3; i++) if (distanceToSource[i] > shortestDistanceToSource) bus[i]->write(shortestDistanceToSource);
+    for (int i =0; i<NUM_CONN; i++) if (distanceToSource[i] > shortestDistanceToSource) bus[i]->write(shortestDistanceToSource);
     addTimer(100, processEnergy);
   }
   // if not, switch off the lamp and drop connectors if we were cut out
   else {
     if (lampState) {
       switchLampOff();
-      for(int i=0; i<3; i++) digitalWrite(coil[i], LOW);
+      for(int i=0; i<NUM_CONN; i++) digitalWrite(coil[i], LOW);
       addTimer(1000, resetConnectors);
     }
   }
   
-  for (int i=0; i<3; i++) distanceToSource[i] = 99;
+  for (int i=0; i<NUM_CONN; i++) distanceToSource[i] = 99;
 }
 
 void setup() {
   // set up pins
-  for (int i=0; i<3; i++) bus[i] = new SoftwareSerialWithHalfDuplex(RX[i], TX[i], false, false);
-  for (int i=0; i<3; i++) pinMode(coil[i], OUTPUT);
+  for (int i=0; i<NUM_CONN; i++) {
+    bus[i] = new SoftwareSerial(RX[i], TX[i]);
+    pinMode(coil[i], OUTPUT);
+  }
   pinMode(leds1pin, OUTPUT);
   pinMode(leds2pin, OUTPUT);
   pinMode(button2pin, INPUT_PULLUP);
   
   // set initial states
-  for (int i=0; i<3; i++) bus[i]->begin(1200);
+  for (int i=0; i<NUM_CONN; i++) bus[i]->begin(1200);
   resetConnectors();
   switchLampOff();
-  for (int i=0; i<3; i++) distanceToSource[i] = 99;
+  for (int i=0; i<NUM_CONN; i++) distanceToSource[i] = 99;
   shortestDistanceToSource = 99;
   alertState = false;
   
@@ -130,12 +134,12 @@ void loop() {
     if (!digitalRead(button2pin)) {
       if(SERIAL_DEBUG) Serial.println("alert");
       char c = ALERT; 
-      for (int i=0; i<3; i++) bus[i]->write(c);
+      for (int i=0; i<NUM_CONN; i++) bus[i]->write(c);
     }
   }
   
   // check all lines for incoming data
-  for (int i=0;i<3;i++) if(bus[i]->available()) {
+  for (int i=0;i<NUM_CONN;i++) if(bus[i]->available()) {
     char c = bus[i]->read();
     if(SERIAL_DEBUG) Serial.println("received: "+c);
     switch(c) {
@@ -145,7 +149,7 @@ void loop() {
           alertState = true;
           alertTimer = addTimer(random(1000, 5000), dropConnector);
           // propagate signal though network
-          for (int j=0; j<3; j++) if (j!=i) bus[j]->write(c);
+          for (int j=0; j<NUM_CONN; j++) if (j!=i) bus[j]->write(c);
         }
         break;
       case DROPPING_CONNECTOR: // some connector in the network is dropped
@@ -153,7 +157,7 @@ void loop() {
         removeTimer(alertTimer);
         alertState = false;
         // propagate signal though network
-        for (int j=0; j<3; j++) if (j!=i) bus[j]->write(c);
+        for (int j=0; j<NUM_CONN; j++) if (j!=i) bus[j]->write(c);
         break;
       default: // energy is received
         if (!isSource) {
@@ -168,4 +172,3 @@ void loop() {
   // update timed processes
   handleTimers();
 }
-
