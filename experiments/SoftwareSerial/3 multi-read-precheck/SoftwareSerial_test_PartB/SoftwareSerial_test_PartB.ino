2d0d7fa02656a0e49febd58dc2550f928321c60e
peteruithoven <peter@peteruithoven.nl>
2016-01-28 18:37:54 +0100

    Improved multi read experiment
    - Removed most unused code
    - Controlling brightness (to check if actual value is received)

--- experiments/SoftwareSerial/3 multi-read-precheck/SoftwareSerial_test_PartB/SoftwareSerial_test_PartB.ino
+++ experiments/SoftwareSerial/3 multi-read-precheck/SoftwareSerial_test_PartB/SoftwareSerial_test_PartB.ino
@@ -1,27 +1,17 @@
 #include <SoftwareSerial.h>
 
-#define NUM_CONN 3 
+#define NUM_CONN 3
 
 SoftwareSerial* bus[NUM_CONN];
 int RX[NUM_CONN] = {A1, A3, A5};
 int TX[NUM_CONN] = {A0, A2, A4};
-
-int coils[NUM_CONN] = {6, 5, 10};
-boolean coilEnabled[NUM_CONN] = {false, false, false};
 
 #define leds1pin 11
 #define leds2pin 3
 
 #define MAX_COUNTER 1000; //4294967295;
 unsigned long counter = MAX_COUNTER;
-boolean ledEnabled = false; 
-
-volatile uint8_t *_rx0PortRegister;
-uint8_t _rx0BitMask;
-volatile uint8_t *_rx1PortRegister;
-uint8_t _rx1BitMask;
-volatile uint8_t *_rx2PortRegister;
-uint8_t _rx2BitMask;
+boolean ledEnabled = false;
 
 int targetBusIndex = 0;
 
@@ -30,88 +20,32 @@ void setup(){
   pinMode(leds2pin, OUTPUT);
   digitalWrite(leds1pin, LOW);
   digitalWrite(leds2pin, LOW);
-  
+
   for (int i=0; i<NUM_CONN; i++) {
     bus[i] = new SoftwareSerial(RX[i], TX[i]);
     bus[i]->begin(9600); //1200
-    pinMode(coils[i], OUTPUT);
   }
-  
+
   Serial.begin(9600);
-  
-  uint8_t port0 = digitalPinToPort(RX[0]);
-  _rx0PortRegister = portInputRegister(port0);
-  _rx0BitMask = digitalPinToBitMask(RX[0]);
-  
-  uint8_t port1 = digitalPinToPort(RX[1]);
-  _rx1PortRegister = portInputRegister(port1);
-  _rx1BitMask = digitalPinToBitMask(RX[1]);
-  
-  uint8_t port2 = digitalPinToPort(RX[2]);
-  _rx2PortRegister = portInputRegister(port2);
-  _rx2BitMask = digitalPinToBitMask(RX[2]);
 }
 
 void loop(){
-  
-  if (!rx0Read()) {
-    updateCoil(0, true);
-    readBus(0);
-  }
-  if (!rx1Read()) {
-    updateCoil(1, true);
-    readBus(1);
-  }
-  if (!rx2Read()) {
-    updateCoil(2, true);
-    readBus(2);
-  }
-  
+  // Forward incomming serial communication to all software serial ports
   if( Serial.available()){
-    counter = MAX_COUNTER;
+    counter = MAX_COUNTER; // light incomming serial communicating led
     int index = int(Serial.read())-48;
+    // int index = Serial.read();
     for (int i=0;i<NUM_CONN;i++) {
-      // bus[i]->write(index);
+      // begin with pulse so other side can start listening
       digitalWrite(TX[i], LOW);
-      delay(1);
+      delay(2);
       digitalWrite(TX[i], HIGH);
-      bus[i]->write(index);  
+      bus[i]->write(index);
     }
-    // digitalWrite(TX[targetBusIndex], LOW);
-    // delay(1);
-    // digitalWrite(TX[targetBusIndex], HIGH);
-    // bus[targetBusIndex]->write(index);
-    
-    // targetBusIndex++;
-    // if(targetBusIndex > 2) targetBusIndex = 0;
   }
+  // update incomming serial communicating led
   updateLed((counter > 0));
   if(counter > 0) counter --;
-}
-
-uint8_t rx0Read() {
-  return *_rx0PortRegister & _rx0BitMask;
-}
-uint8_t rx1Read() {
-  return *_rx1PortRegister & _rx1BitMask;
-}
-uint8_t rx2Read() {
-  return *_rx2PortRegister & _rx2BitMask;
-}
-
-void readBus(uint8_t index) {
-  bus[index]->listen();
-  if( bus[index]->available() ){
-    char c = bus[index]->read();
-    Serial.write(c);
-  }
-}
-
-void updateCoil(uint8_t pinIndex, boolean enable) {
-  if(enable != coilEnabled[pinIndex]) {
-    digitalWrite(coils[pinIndex], enable? HIGH : LOW);
-    coilEnabled[pinIndex] = enable;
-  }
 }
 
 void updateLed(boolean enable) {
--- experiments/SoftwareSerial/3 multi-read-precheck/SoftwareSerial_test_partA/SoftwareSerial_test_partA.ino
+++ experiments/SoftwareSerial/3 multi-read-precheck/SoftwareSerial_test_partA/SoftwareSerial_test_partA.ino
@@ -10,7 +10,7 @@ int coil[NUM_CONN] = {6, 5, 10};
 #define leds1pin 11
 #define leds2pin 3
 
-// ToDo: turn into arrays / enums: 
+// ToDo: turn into arrays / enums:
 volatile uint8_t *_rx0PortRegister;
 uint8_t _rx0BitMask;
 volatile uint8_t *_rx1PortRegister;
@@ -31,28 +31,30 @@ void setup(){
     bus[i]->begin(9600); //1200
     pinMode(coil[i], OUTPUT);
   }
-  
+
   pinMode(leds1pin, OUTPUT);
   pinMode(leds2pin, OUTPUT);
-  
+
   digitalWrite(leds1pin, LOW);
   digitalWrite(leds2pin, LOW);
-  
+
   uint8_t port0 = digitalPinToPort(RX[0]);
   _rx0PortRegister = portInputRegister(port0);
   _rx0BitMask = digitalPinToBitMask(RX[0]);
-  
+
   uint8_t port1 = digitalPinToPort(RX[1]);
   _rx1PortRegister = portInputRegister(port1);
   _rx1BitMask = digitalPinToBitMask(RX[1]);
-  
+
   uint8_t port2 = digitalPinToPort(RX[2]);
   _rx2PortRegister = portInputRegister(port2);
   _rx2BitMask = digitalPinToBitMask(RX[2]);
+
+  Serial.begin(9600);
 }
 
 void loop(){
-  
+
   if (activeBus==-1) {
     if (!rx0Read()) {
       setActiveBus(0);
@@ -83,23 +85,36 @@ void setActiveBus(int index) {
   activeBus = index;
   listenStartTime = millis();
   bus[index]->listen();
-  // SoftwareSerial::handle_interrupt(); // not possible? So requires sending a pulse before sending message. 
+  // SoftwareSerial::handle_interrupt(); // not possible? So requires sending a pulse before sending message.
 }
 
 void readActiveBus() {
-  if( bus[activeBus]->available() ){
-    toggleCoil(activeBus);
+  //if( bus[activeBus]->available() ){
+  while( bus[activeBus]->available() ){
+    // only toggle bus's led when index is myID ?
     int index = int(bus[activeBus]->read());
-    // if( index == myID ){
-    //   bus[index]->print("I am ");
-    //   bus[index]->println(myID);
-    // }
-    activeBus = -1;
+    if(index != 0) {
+      // Serial.write(index);
+      Serial.println(index);
+      //if( index == myID ){
+        // int brightness = index*100/10*255/100;
+        int brightness = float(index)/10*255;
+        //toggleCoil(activeBus, brightness);
+        setCoil(activeBus, brightness);
+      //   bus[index]->print("I am ");
+      //   bus[index]->println(myID);
+      //}
+      // since we're only sending one thing, after reading it we start checking other ports again
+      activeBus = -1;
+      break;
+    }
   }
   if (millis()-listenStartTime > listenTimeout) activeBus = -1;
 }
 
-
 void toggleCoil(int index) {
   digitalWrite(coil[index], !digitalRead(coil[index]));
+}
+void setCoil(int index, int value) {
+  analogWrite(coil[index], value);
 }
